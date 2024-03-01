5e50abbc156085e746b485712873f86fcd185934
pyusuket <101777200+pyusuket@users.noreply.github.com>
2024-03-01 16:29:46 +0000

    [Update] ダイレクトメッセージの更新

--- app/controllers/user/messages_controller.rb
+++ app/controllers/user/messages_controller.rb
@@ -9,6 +9,7 @@ class User::MessagesController < ApplicationController
     @user = User.find(params[:user_id])
     @message = Message.new
     @messages = Message.where(sender_id: [@user_current.id, @user.id], receiver_id: [@user_current.id, @user.id]).order(created_at: :desc)
+    @messages.where(receiver_id: @user_current.id, read: false).update_all(read: true)
   end
   
   def create
@@ -16,13 +17,12 @@ class User::MessagesController < ApplicationController
   @message.sender_id = current_user.id
   @message.receiver_id = params[:user_id] 
 
-  if @message.save
-    redirect_to user_user_message_path(@message.receiver_id, @message.id)
-  else
-    # メッセージの送信に失敗した場合の処理
-    flash.now[:alert] = "メッセージの送信に失敗しました。"
-    render :show
-  end
+    if @message.save
+      redirect_to user_user_message_path(@message.receiver_id, @message.id)
+    else
+      flash.now[:alert] = "メッセージの送信に失敗しました。"
+      render :show
+    end
   end
   
   private
