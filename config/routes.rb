c048d640751aae0072487c14a756cfcffa4f1dfe
pyusuket <101777200+pyusuket@users.noreply.github.com>
2024-03-07 12:54:16 +0000

    デプロイ作業中エラー修正のアプリケーションルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,27 @@
 Rails.application.routes.draw do
-  get 'events', to: "user/events#index"
-  root 'user/homes#mypage'
-  
-  devise_scope :user do
-    get  'users/sign_out',      to: 'user/sessions#destroy'
-    post "users/guest_sign_in", to: "user/sessions#guest_sign_in"
-  end
-  
+   # Devise routes
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "user/registrations",
     sessions:      'user/sessions'
   }
-  
-  # userのルーティング
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # User routes
   namespace :user do
-    get '/homes/mypage' => 'homes#mypage', as: 'homes_mypage'
+    get '/homes/mypage', to: 'homes#mypage', as: 'homes_mypage'
     resources :users, only: [:index, :show, :edit, :update, :destroy] do
       member do 
         get "nices" 
       end 
-      resource :follow,         only: [:create, :destroy]
+      resource :follow, only: [:create, :destroy]
       get "followings" => "follows#followings", as: "followings"
       get "followers"  => "follows#followers", as: "followers"
-      resources :messages,      only: [:index, :show, :create] 
+      resources :messages, only: [:index, :show, :create] 
       resources :notifications, only: [:index, :show] 
       resources :events 
-  end
+    end
     resources :posts do
       resources :comments, only: [:create]
       resource  :nices,    only: [:create, :destroy]
@@ -33,14 +29,17 @@ Rails.application.routes.draw do
     end
   end
   
-  # adminのルーティング
-  devise_for :admins, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
-  
+  # Admin routes
   namespace :admin do
-    get '/homes/top' => 'homes#top', as: 'homes_top'
+    get '/homes/top', to: 'homes#top', as: 'homes_top'
     resources :users
     resources :posts
+  end
+
+  # Devise scopes
+  devise_scope :user do
+    get  'users/sign_out', to: 'user/sessions#destroy'
+    post "users/guest_sign_in", to: "user/sessions#guest_sign_in"
+    root 'user/sessions#new'
   end
 end
